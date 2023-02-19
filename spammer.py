51d5925eac13543e7893e0b41abca84f955a63c0
LeeTxHacker <118864942+leetxhacker@users.noreply.github.com>
2023-02-19 23:36:18 +0530

    Create spammer.py

---
+++ spammer.py
@@ -0,0 +1,7 @@
+import pyautogui
+import time
+# <------ LeeTxHacker ------> #
+while True:
+    pyautogui.typewrite("Hey, This Is PyAutoGui Module Test Spammer") # Edit Your Message
+    pyautogui.press("enter")
+    time.sleep(1) # You Can Edit This Interval
